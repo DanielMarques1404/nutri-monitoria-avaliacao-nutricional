0f0642666d798a815117b2e0985b544d5bc28d91
daniel <daniel.marques.bnb@gmail.com>
2026-04-25 15:00:38 -0300

    Admin Navigation - first commit

--- src/components/admin/TagForm.tsx
+++ src/components/admin/TagForm.tsx
@@ -36,10 +36,8 @@ export const TagForm = () => {
   };
 
   return (
-    <section>
-      <h1>TAGs</h1>
-
-      <form onSubmit={handleSubmit(submit)}>
+    <section className="flex flex-col gap-2 w-1/2 border-2 border-mediumGrey p-4 rounded-md">
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(submit)}>
         <Input
           label={"TAG"}
           type="text"
@@ -48,11 +46,13 @@ export const TagForm = () => {
           {...register("name")}
         />
         <Button
-          classname="inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded-md text-lg"
+          classname="text-white border-0 py-2 px-6 focus:outline-none rounded-md text-lg ml-auto"
           type="submit"
           label="Adicionar"
         />
       </form>
+
+      <div className="border-2 border-dark-green my-2"></div>
 
       <ul>
         {tagsList?.map((tag) => (
--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import { CategoryForm } from "../../components/admin/CategoryForm";
 import { TagForm } from "../../components/admin/TagForm";
+import { Button } from "../../components/ui/Button";
 
 export const Admin = () => {
+  const [entity, setEntity] = useState<"tag" | "category" | "question">("tag");
   return (
     <section className="flex flex-col gap-4 p-8">
       <h1>Admin</h1>
-      <TagForm />
-      <CategoryForm />
+
+      <nav className="grid grid-cols-3 gap-2 text-white">
+        <Button
+          classname={`${entity === "tag" ? "bg-light-green text-white" : "bg-dark-green text-white"}`}
+          label="TAGs"
+          onClick={() => setEntity("tag")}
+        />
+        <Button
+          classname={`${entity === "category" ? "bg-light-green text-white" : "bg-dark-green text-white"}`}
+          label="Categorias"
+          onClick={() => setEntity("category")}
+        />
+        <Button
+          classname={`${entity === "question" ? "bg-light-green text-white" : "bg-dark-green text-white"}`}
+          label="Questões"
+          onClick={() => setEntity("question")}
+        />
+      </nav>
+      <div className="flex items-center justify-center py-2 px-6 w-full">
+        {entity === "tag" && <TagForm />}
+        {entity === "category" && <CategoryForm />}
+        {/* {entity === "question" && <QuestionForm />} */}
+      </div>
     </section>
   );
 };
