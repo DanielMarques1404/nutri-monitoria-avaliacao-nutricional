6f595563208c554c2fe283e2445d849187552506
daniel <daniel.marques.bnb@gmail.com>
2026-05-17 20:59:59 -0300

    Vitest - first commit

---
+++ src/test/ex.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "vitest";
+import { QuestionTagsUseCases } from "../domain/useCases/QuestionTagsUseCase";
+import { QuestionUseCases } from "../domain/useCases/QuestionUseCases";
+import { RepositoryFactory } from "../infra/factory/RepositoryFactory";
+import { CURRENT_QUESTIONNAIRE, CURRENT_TECH_REPOSITORY } from "../utils/data";
+import type { IQuestion } from "../domain/entities/entities";
+
+const questionTagsRepo = RepositoryFactory.getRepo(
+  CURRENT_TECH_REPOSITORY,
+).createQuestionTagsRepo();
+const questionOptionsRepo = RepositoryFactory.getRepo(
+  CURRENT_TECH_REPOSITORY,
+).createQuestionOptionsRepo();
+const questionRepo = RepositoryFactory.getRepo(
+  CURRENT_TECH_REPOSITORY,
+).createQuetionsRepo();
+
+const ucQTags = new QuestionTagsUseCases(questionTagsRepo);
+const ucQuestions = new QuestionUseCases(
+  questionRepo,
+  questionTagsRepo,
+  questionOptionsRepo,
+);
+
+// describe('Recuperar Tags', () => {
+//   it('deve recuperar as Tags de uma questão', async () => {
+
+//     const x = await ucQTags.listTagsByQuestionId(1)
+//     expect(x?.length).toBe(2);
+//   });
+// });
+
+describe("Tratamento para Question", () => {
+  it("Recuperando Question TESTE", async () => {
+    const x: IQuestion[] | null = await ucQuestions.listByQuestionnaireId(CURRENT_QUESTIONNAIRE);
+    expect(x && x[0].title).toBe("TITLE TESTE");
+    // console.log({...x[0], id: 0})
+    // const newQuestion = {...x![0], id: 0}
+    // const y = await ucQuestions.createOrUpdate(newQuestion)
+  });
+});
