21dcdadf8bedd47eb5a39016ead2905a4d0dba2f
daniel <daniel.marques.bnb@gmail.com>
2026-08-22 15:32:21 -0300

    Answer component refactoring

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -22,6 +22,8 @@ export function Answer({
 }: AnswerProps) {
   const isSelected = selectedOptionId === option.id;
   const isCorrect = correctOption === option.id;
+  const showUserAnswerBadge = isSelected;
+  const showCorrectAnswerBadge = isAnswerRevealed && isCorrect;
 
   return (
     <div
@@ -31,21 +33,29 @@ export function Answer({
         onSelect(questionId, option.id);
       }}
       className={cn(
-        "flex flex-col w-52 h-52 bg-white rounded-xl border border-medium-green",
+        "flex flex-col w-52 h-52 overflow-hidden bg-white rounded-xl border border-medium-green",
         isAnswerRevealed ? "cursor-default" : "cursor-pointer",
         !isAnswerRevealed &&
           !isSelected &&
           "hover:border-4 hover:border-dark-green",
-        isAnswerRevealed && isCorrect && "card",
-        isSelected && "border-4 border-orange ",
       )}
     >
       <h1 className="text-sm text-center tracking-widest title-font mb-1 font-medium bg-light-green rounded-t-lg">
         {letter}
       </h1>
-      <span className="font-ubuntu text-dark-green text-sm mx-3 my-1 ">
+      <span className="flex-1 font-ubuntu text-dark-green text-sm mx-3 my-1 overflow-auto">
         {option.description}
       </span>
+      {(showUserAnswerBadge || showCorrectAnswerBadge) && (
+        <div className="mt-auto flex flex-col text-center text-[0.68rem] font-bold uppercase tracking-wide text-white">
+          {showUserAnswerBadge && (
+            <span className="bg-orange px-2 py-1">Sua resposta</span>
+          )}
+          {showCorrectAnswerBadge && (
+            <span className="bg-light-green px-2 py-1">Resposta correta</span>
+          )}
+        </div>
+      )}
     </div>
   );
 }
